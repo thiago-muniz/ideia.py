4170a098c5206ab73dab0affc1b11c998bfa075b
Thiago Muniz <thiagotsocial@gmail.com>
2020-09-25 19:16:33 -0300

    manipulação de arquivos

---
+++ Criador de fichas cadastrais/Criador de fichas cadastrais.py
@@ -0,0 +1,10 @@
+numalunos = int(input('Quantos novos alunos vão abrir fichas de cadastro? '))
+
+for c in range(1, numalunos+1):
+    nome = str(input(f'Nome do aluno {c}: '))
+    idade = int(input(f'Idade de {nome}: '))
+    texto = open(f'Aluno {nome}', 'wt')
+    texto.write(f'Nome: {nome}\n')
+    texto.write((f'Idade: {idade}'))
+    texto.close()
+
